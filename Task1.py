20029e7aad9134028edc92550d54329f12d4bf6f
Sorokin Mikhail <ip.sorokin1738@mail.ru>
2022-10-25 19:55:58 +0300

    added code in Task1

--- Task1.py
+++ Task1.py
@@ -4,6 +4,20 @@
 
 # - при d = 0.001, π = 3.141.    10^(-1) ≤ d ≤10^(-10)
 
-d=0.001
-pi=
+d=int(input('Введите число : '))
+d=d/1000
+pi=0
+sign=1
+x=1
+while True:
+    a=4/x
+    pi+=sign*a
+    print(pi)
+    if a<=d:
+     break
+    sign=-sign
+    x+=2
+    print(f'Множитель = {x}')
+print(round(pi,3))
+print(d)
 
--- Task2.py
+++ Task2.py
@@ -4,6 +4,6 @@ N = int(input('N :'))
 numlist=[]
 for i in range(1,N):
     if (N % i==0):
-        if(i% i==0)and(i%1==0):
+        if(i % i==0)and(i%1==0):
             numlist.append(i)
 print(f'Простые множетели N-го числа => {numlist}')
