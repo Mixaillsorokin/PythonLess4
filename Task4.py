444424eb096e91038870ae4ed72ec130d1226e07
Sorokin Mikhail <ip.sorokin1738@mail.ru>
2022-10-25 17:57:25 +0300

    Added code in task4

--- Task4.py
+++ Task4.py
@@ -4,3 +4,24 @@
 # Пример:
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+import random
+k=int(input())
+KmN=[]
+for i in range(k+1):
+    KmN.append(random.randint(1,99))
+print(KmN)
+ForFile=''
+for i in range(len(KmN)-1):
+    if KmN[i]!=0:
+        if(k-i)==1:
+            ForFile+=str(KmN[i])+'x +'
+        else:
+            f=int(k-i)
+            ForFile+=str(KmN[i])+'x'+'^'+str(f)+' + '
+
+ForFile+=str(KmN[-1])+'= 0'
+
+with open('Task.txt', 'w+') as date:
+    date.write(ForFile)
+
+print(ForFile)
